d327b16ce208d45ddf03ec2a72701a7dfa5c07c3
schrutek <martin@schrutek>
2023-09-26 19:50:45 +0200

    First Commit from IDE Visual Studio

---
+++ Spg.TypesDemo/src/Spg.TypesDemo.App/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+
+int i = 12;
+
+Console.WriteLine(i);
+
+Person p = new Person();
+p.Id = 4711;
+p.Name= "Test";
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ Spg.TypesDemo/src/Spg.TypesDemo.DoaminModel/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.TypesDemo.DoaminModel.Model
+{
+    public class Product
+    {
+
+    }
+}
