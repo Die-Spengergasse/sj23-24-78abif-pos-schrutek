edef72ecbc7b11e0d7d467b5cccea4de40b98afd
schrutek <martin@schrutek>
2023-09-28 20:46:09 +0200

    Basic Domain Model created
    * Empty Entities created
    
    Siggned by: SRM (schrutek@spengergasse.at)

--- Spg.TypesDemo/src/Spg.TypesDemo.App/Program.cs
+++ Spg.TypesDemo/src/Spg.TypesDemo.App/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Spg.TypesDemo.DoaminModel.Model;
+using System.Net.Http.Headers;
+
+Console.WriteLine("Hello, World!");
 
 int i = 12;
 
@@ -7,6 +10,10 @@ Console.WriteLine(i);
 Person p = new Person();
 p.Id = 4711;
 p.Name= "Test";
+
+Product product = new Product();
+
+string s = Console.ReadLine();
 
 class Person
 {
---
+++ Spg.TypesDemo/src/Spg.TypesDemo.DoaminModel/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.TypesDemo.DoaminModel.Model
+{
+    internal class Class
+    {
+        public void SetClass(Class @class)
+        {
+            @class.SetClass(this);
+        }
+    }
+}
